b9eb4e00d1da7ce1207203e6f9aa52e120a17110
Roman Os <roman.osoviak2@gmail.com>
2023-01-10 00:03:58 +0200

    Added placeholders methods and started with pylint approach

--- pages/login_page.py
+++ pages/login_page.py
@@ -13,3 +13,34 @@ class LoginPage(BasePage):
     def go_to_site(self):
         """Open login page"""
         return self.driver.get(self.base_url + self._URL_PATH)
+
+    def login(self, email: str, password: str):
+        """
+       Positive login procedure
+
+       :param email: email address to login with
+       :param password:  password to login with
+       :return:
+       """
+        self.set_email(email)
+        self.set_password(password)
+        self.click_login_btn()
+
+    def set_email(self, email: str):
+        """
+        Input email field
+
+        :param email: email address need to enter
+        :return:
+        """
+
+    def set_password(self, password: str):
+        """
+        Input password field
+
+        :param password: password field need to enter
+        :return:
+        """
+
+    def click_login_btn(self):
+        """Click on Login btn"""
--- tests/tests_ui/login/test_login.py
+++ tests/tests_ui/login/test_login.py
@@ -2,7 +2,6 @@
 Module with test case for testing login is successful
 """
 
-from selenium import webdriver
 from pages.login_page import LoginPage
 from utils.constants import PageTitle
 
@@ -10,7 +9,13 @@ from utils.constants import PageTitle
 def test_login(browser, get_env):
     """Basic login success flow"""
     site_url = get_env['environment']['site_url']
+    email = get_env['user']['existing_email']
+    password = get_env['user']['password']
+
     class_page = LoginPage(browser, site_url)
     class_page.go_to_site()
 
     assert browser.title == PageTitle.LOGIN_PAGE_TITLE
+
+    class_page.login()
+    assert browser.title == PageTitle.MY_ACCOUNT_PAGE_TITLE
