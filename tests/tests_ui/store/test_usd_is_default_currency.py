4e0564e8adf9ea74acb24d4777025683edca2303
Roman Os <roman.osoviak2@gmail.com>
2023-02-03 21:28:20 +0200

    Fixed test for checking default currency

--- tests/tests_ui/store/test_usd_is_default_currency.py
+++ tests/tests_ui/store/test_usd_is_default_currency.py
@@ -1,5 +1,6 @@
 """Test Module for verifying that default currency is USD"""
 from pages.store_page import StorePage
+from utils.enums import CurrencyTemp
 
 
 def test_default_currency_is_usd(browser, get_env):
@@ -7,4 +8,4 @@ def test_default_currency_is_usd(browser, get_env):
     site_url = get_env['environment']['site_url']
     store_page = StorePage(browser, site_url)
     store_page.go_to_site()
-    assert store_page.verify_selected_currency_dropdown()
+    store_page.verify_selected_currency_dropdown(CurrencyTemp.USD)
