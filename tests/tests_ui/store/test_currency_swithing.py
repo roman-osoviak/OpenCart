1480f4a94392f6a701fd6779cbcd17e6bbab5021
Roman Os <roman.osoviak2@gmail.com>
2023-02-04 15:45:57 +0200

    Added verifying also for currency drop-down sign

--- tests/tests_ui/store/test_currency_swithing.py
+++ tests/tests_ui/store/test_currency_swithing.py
@@ -13,18 +13,18 @@ def test_currency_switching(browser, get_env):
 
     # select EURO and check certain text
     store_page.select_currency(Currency.EURO)
-
+    # on shopping cart button
     store_page.verify_shopping_cart_button_text(StoreCurrency.SHOPPING_CART_BUTTON_ZERO_COST_EURO)
+    # and for currency drop-down sign
+    store_page.verify_selected_currency_dropdown(Currency.EURO)
 
     # select POUNDS and check certain text
     store_page.select_currency(Currency.POUNDS)
-
     store_page.verify_shopping_cart_button_text(StoreCurrency.SHOPPING_CART_BUTTON_ZERO_COST_POUND)
+    store_page.verify_selected_currency_dropdown(Currency.POUNDS)
 
     # select USD and check certain text
     store_page.select_currency(Currency.USD)
-    # assert store_page.verify_selected_currency_dropdown()
-
     store_page.verify_shopping_cart_button_text(StoreCurrency.SHOPPING_CART_BUTTON_ZERO_COST_USD)
     # we need to check if sign before currency dropdown is also changed
     store_page.verify_selected_currency_dropdown(Currency.USD)
