c1d19b38f21335296d2436a965bedcc65cd96204
Roman Os <roman.osoviak2@gmail.com>
2023-03-01 17:08:23 +0200

    Layout for testing upload file flow

---
+++ tests/tests_ui/product_details/test_upload_file.py
@@ -0,0 +1,9 @@
+"""Module for testing upload file positive flow"""
+from pages.product_details_page import ProductDetailsPage
+
+
+def test_upload_file(browser, get_env):
+    """Verifying that file can be uploaded"""
+    site_url = get_env['environment']['site_url']
+    prod_details_page = ProductDetailsPage(browser, site_url)
+    prod_details_page.click_on_upload_button()
