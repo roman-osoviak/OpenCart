6568b9fa4f9ff4724c7882f1b46f20426b83c2cc
Roman Os <roman.osoviak2@gmail.com>
2023-03-08 22:44:23 +0200

    Added allure steps into tab navigation test

--- tests/tests_ui/product_details/test_tab_navigation.py
+++ tests/tests_ui/product_details/test_tab_navigation.py
@@ -1,4 +1,6 @@
 """Module for testing low tabs navigation"""
+import allure
+
 from pages.product_details_page import ProductDetailsPage
 from utils.enums import ProductDetailsTabsLower
 
@@ -6,15 +8,18 @@ from utils.enums import ProductDetailsTabsLower
 def test_tab_navigation(browser, get_env):
     """Verifying that navigation through tabs works as expected"""
     site_url = get_env['environment']['site_url']
-    prod_details_page = ProductDetailsPage(browser, site_url)
-    prod_details_page.go_to_site()
+
+    with allure.step(f'Opening {site_url}'):
+        prod_details_page = ProductDetailsPage(browser, site_url)
+        prod_details_page.go_to_site()
 
     # check navigation through the tabs
-    prod_details_page \
-        .verify_tab_is_active(ProductDetailsTabsLower.TAB_DESCRIPTION) \
-        .click_on_tab_lower(ProductDetailsTabsLower.TAB_SPECIFICATION) \
-        .verify_tab_is_active(ProductDetailsTabsLower.TAB_SPECIFICATION) \
-        .click_on_tab_lower(ProductDetailsTabsLower.TAB_REVIEWS) \
-        .verify_tab_is_active(ProductDetailsTabsLower.TAB_REVIEWS) \
-        .click_on_tab_lower(ProductDetailsTabsLower.TAB_DESCRIPTION) \
-        .verify_tab_is_active(ProductDetailsTabsLower.TAB_DESCRIPTION)
+    with allure.step('Checking tabs navigation'):
+        prod_details_page \
+            .verify_tab_is_active(ProductDetailsTabsLower.TAB_DESCRIPTION) \
+            .click_on_tab_lower(ProductDetailsTabsLower.TAB_SPECIFICATION) \
+            .verify_tab_is_active(ProductDetailsTabsLower.TAB_SPECIFICATION) \
+            .click_on_tab_lower(ProductDetailsTabsLower.TAB_REVIEWS) \
+            .verify_tab_is_active(ProductDetailsTabsLower.TAB_REVIEWS) \
+            .click_on_tab_lower(ProductDetailsTabsLower.TAB_DESCRIPTION) \
+            .verify_tab_is_active(ProductDetailsTabsLower.TAB_DESCRIPTION)
