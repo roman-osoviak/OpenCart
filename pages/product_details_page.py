e965ace1f3b481d360362a484e6783b0509d0275
Roman Os <roman.osoviak2@gmail.com>
2023-03-16 20:49:37 +0200

    Added allure approach to add to compare tests

--- pages/product_details_page.py
+++ pages/product_details_page.py
@@ -87,6 +87,7 @@ class ProductDetailsPage(BasePage):
     def click_on_add_compare_button(self):
         """Method that clicks on add to compare list"""
         self.click_on_element(ProductDetailsLocators.ADD_TO_COMPARE_BUTTON)
+        return self
 
     def click_on_brand(self):
         """Method clicks on Apple brand link"""
--- tests/tests_ui/product_details/test_add_to_compare.py
+++ tests/tests_ui/product_details/test_add_to_compare.py
@@ -1,4 +1,6 @@
 """Module for testing clicking on 'Compare This Product' button"""
+import allure
+
 from pages.product_details_page import ProductDetailsPage
 
 
@@ -6,10 +8,12 @@ def test_click_on_compare_product_button_unauthorised_user(browser, get_env):
     """Test flow with adding item to the compare list as unauthorised user"""
     site_url = get_env['environment']['site_url']
     prod_details_page = ProductDetailsPage(browser, site_url)
-    prod_details_page.go_to_site()
-
-    prod_details_page.verify_alert_is_displayed(False)
-    prod_details_page.click_on_add_compare_button()
-    prod_details_page.verify_alert_is_displayed()
+    with allure.step(f'Opening {site_url}'):
+        prod_details_page.go_to_site()
+    with allure.step('Testing if alert window is shown'):
+        prod_details_page \
+            .verify_alert_is_displayed(False) \
+            .click_on_add_compare_button() \
+            .verify_alert_is_displayed()
     # need to check if opacity window shows
     assert 'Success: You have added' in prod_details_page.get_alert_text()
